64b804290c1616fff708666e174df042f2889a0d
Marcelo Magalhães <147987781+MBrunoMSM@users.noreply.github.com>
2024-11-09 11:39:11 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de Dados")
+
+# Variáveis
+Nome = "Bruno"
+Idade = 24
+
+# Print
+print(Nome)
+
+# Lista
+Lista = [1,2,3,4,5]
+
+#Print Lista
+print(Lista)
+
+# Soma
+Idade_Nova = Idade + 1
+print(Idade_Nova)
+
+# Lista inversa
+lista_inversa = Lista.reverse()
